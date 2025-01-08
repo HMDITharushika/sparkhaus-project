7151b0b2402fea7f25e1e3a57f99779b8175b4e6
ishadiIFSolutions <76896498+HMDITharushika@users.noreply.github.com>
2025-01-08 23:38:51 +0530

    Navbar - Part 1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,80 @@
-const Navbar = () => {
-  return (
-    <div>
-      Nav naw nav
-    </div>
-  )
-}
+import { useState } from "react";
+import logo from "../assets/images/sparkhaus.png";
+import { FaBars, FaTimes } from "react-icons/fa";
 
-export default Navbar
+const NavBar = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  const handleScroll = (event, targetId) => {
+    event.preventDefault();
+    const targetElement = document.getElementById(targetId);
+    if (targetElement) {
+      const offsetTop = targetElement.offsetTop - 80;
+      window.scrollTo({
+        top: offsetTop,
+        behavior: "smooth",
+      });
+    }
+    setIsMobileMenuOpen(false);
+  };
+
+  const LINKS = [
+    { text: "Home", targetId: "home" },
+    { text: "Services", targetId: "services" },
+    { text: "Blogs", targetId: "blog" },
+    { text: "Contact Us", targetId: "contactus" },
+  ];
+
+  return (
+    <nav className="fixed top-4 z-50 flex w-full flex-col items-center justify-center font-bebas-neue rounded-3xl">
+      <div className="flex w-full items-center justify-between overflow-y-hidden p-4 backdrop-blur-lg lg:m-2 lg:w-[50rem] lg:rounded-full lg:shadow-lg">
+        
+          
+          <img src={logo} alt="logo" className="w-32 h-10" />
+         
+
+        {/* Mobile View */}
+        <div className="hidden space-x-6 lg:flex">
+          {LINKS.map((link, index) => (
+            <a
+              key={index}
+              href={`#${link.targetId}`}
+              className={`text-sm ${
+                index !== 0 ? "border-l-2 border-neutral-300/20 pl-2" : ""
+              } hover:opacity-50`}
+              onClick={(e) => handleScroll(e, link.targetId)}
+            >
+              {link.text}
+            </a>
+          ))}
+        </div>
+
+        <div className="lg:hidden">
+          <button onClick={toggleMobileMenu}>
+            {isMobileMenuOpen ? <FaTimes /> : <FaBars />}
+          </button>
+        </div>
+      </div>
+      {isMobileMenuOpen && (
+        <div className="w-full backdrop-blur-lg lg:hidden">
+          {LINKS.map((link, index) => (
+            <a
+              key={index}
+              href={`#${link.targetId}`}
+              className="block p-4 uppercase tracking-tighter"
+              onClick={(e) => handleScroll(e, link.targetId)}
+            >
+              {link.text}
+            </a>
+          ))}
+        </div>
+      )}
+    </nav>
+  );
+};
+
+export default NavBar;
